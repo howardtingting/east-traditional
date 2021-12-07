91cac96ed26fe4d4aca877cb0cf99719d4e588ad
Howard Ting <howardtingting@gmail.com>
2021-12-06 17:20:44 -0800

    created and half-styled navbar using mui themes

--- components/generic-page-components/NavbarLanding/Main.tsx
+++ components/generic-page-components/NavbarLanding/Main.tsx
@@ -1,5 +1,40 @@
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import theme from '../../mui-themes/MainThemes';
+import styles from './Navbar.module.css';
+import { ThemeProvider } from '@emotion/react';
+
+const DefaultReturn = (<div>generic-page-components/NavbarLanding/Main.tsx</div>);
+
+const Logo = (
+  <div>
+    EastTraditionalLogo
+  </div>
+);
+
+const NavListItems = [
+  (<Link className={styles.navlink} href="landing" underline={'hover'}>About</Link>),
+  (<Link className={styles.navlink} href="landing" underline={'hover'}>Cases Treated</Link>),
+  (<Link className={styles.navlink} href="landing" underline={'hover'}>Contact</Link>),
+  (<Button className={styles.navbutton} variant="contained" color={'navgreen'}>Login</Button>),
+  (<Button className={styles.navbutton} variant="contained" color={'navgreen'} href="bookonline">Book Appointment</Button>)
+];
+
+const NavContainer = (
+  <Stack spacing={3} direction="row">
+    {Logo}
+    {[...NavListItems]}
+  </Stack>
+);
+
 function NavbarLanding() {
-    return <div>generic-page-components/NavbarLanding/Main.tsx</div>
-  }
+  return (
+    <ThemeProvider theme={theme}>
+      {NavContainer}
+    </ThemeProvider>
+  );
+};
   
 export default NavbarLanding
---
+++ components/mui-themes/MainThemes.ts
@@ -0,0 +1,66 @@
+import { createTheme } from '@mui/material/styles';
+
+// declare module '@mui/material/styles' {
+//     interface Theme {
+//         etm_palette: {
+//             white: string;
+//             bluelink: "2D9CDB";
+//             lightgreen: "6FCF97";
+//             actiongreen: "04C35C";
+//         };
+//     }
+//     // allow configuration using `createTheme`
+//     interface ThemeOptions {
+//         etm_palette?: {
+//             white?: string;
+//             bluelink: "2D9CDB";
+//             lightgreen: "6FCF97";
+//             actiongreen: "04C35C";
+//         };
+//     }
+// }
+
+// const theme = createTheme({
+//     etm_palette: {
+//       white: "#fff",
+//       bluelink: "2D9CDB",
+//       lightgreen: "6FCF97",
+//       actiongreen: "04C35C"
+//     },
+// });
+
+const theme = createTheme({
+    palette: {
+      navgreen: {
+        main: '#6FCF97',
+        contrastText: '#fff',
+      },
+      whitelink: {
+          main: '#fff',
+          contrastText: 'rgb(255,255,255,0)',
+      },
+    },
+  });
+  
+  declare module '@mui/material/styles' {
+    interface Palette {
+      navgreen: Palette['primary'];
+      whitelink: Palette['primary'];
+    }
+  
+    // allow configuration using `createTheme`
+    interface PaletteOptions {
+      navgreen?: PaletteOptions['primary'];
+      whitelink?: PaletteOptions['primary'];
+    }
+  }
+  
+  // Update the Button's color prop options
+  declare module '@mui/material/Button' {
+    interface ButtonPropsColorOverrides {
+      navgreen: true;
+      whitelink: true;
+    }
+  }
+
+export default theme;
