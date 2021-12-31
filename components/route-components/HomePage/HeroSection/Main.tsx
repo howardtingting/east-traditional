ef441ead409f8ec4bdb6e841afec66932c088828
Howard Ting <howardtingting@gmail.com>
2021-12-30 17:45:48 -0800

    Landing Page Hero section completed: action call made mobile-responsive"

--- components/route-components/HomePage/HeroSection/Main.tsx
+++ components/route-components/HomePage/HeroSection/Main.tsx
@@ -37,10 +37,24 @@ const HeroSection = (props: any) => {
       Traditional Chinese Medicine, Acupuncture &#38; Auricular Medicine
     </Typography>
   );
+
+  //HERO ACTION CALL
+  const HeroActionCallExpandedSx = {
+    fontSize:"1rem"
+  }
+  const HeroActionCallMobileSx = {
+    fontSize:"0.8rem"
+  }
+  const HeroActionCallSx = Object.assign({},
+    (size.width || ((typeof window !== "undefined") ? window.innerWidth : 0)) > 867 ? HeroActionCallExpandedSx : HeroActionCallMobileSx
+  );
   const HeroActionCall = (
-    <Box sx={{border:'1px solid green'}}>
-      <Typography>
-        <Link href="bookonline">Book Online</Link>, call 408-372-5492, or email <Link href="/">jenlee@east-medicine.com</Link> to schedule
+    <Box className={HeroActionCallSx} sx={{border:'1px solid green', backgroundColor:"#E2E2E2", margin:"20px clamp(10%, 20px, 20px)"}}>
+      <Typography sx={{whiteSpace:"normal", display: "flex", justifyContent:"center", flexWrap:"wrap", padding:"20px"}}>
+        <span style={{whiteSpace: "nowrap"}}><Link href="bookonline" className={styles.link}>Book Online</Link>,</span>&nbsp;
+        <span style={{whiteSpace: "nowrap"}}>call 408-372-5492, or</span>&nbsp;
+        <span style={{whiteSpace: "nowrap"}}>email <Link href="/" className={styles.link}>jenlee@east-medicine.com</Link></span>&nbsp;
+        <span>to schedule</span>
       </Typography>
     </Box>
   );
