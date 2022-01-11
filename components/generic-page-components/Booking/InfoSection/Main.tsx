64cc5a6e8c0fa0e2b072b5c914c4cdfeb69ff7e7
Howard Ting <howardtingting@gmail.com>
2022-01-11 13:31:25 -0800

    Info Section of Appointment booking completed

--- components/generic-page-components/Booking/InfoSection/Main.tsx
+++ components/generic-page-components/Booking/InfoSection/Main.tsx
@@ -10,9 +10,9 @@ import style from "Info.module.css";
 import { Typography } from "@mui/material";
 
 
-const IconWithText = (args: any) => {
+const IconWithText = (args: {icon?: any, sx?: {[x:string]: any}, text: any[]}) => {
     const IconComponent = args.icon || '';
-    const text: string[] = args.text ? args.text : ['default text'];
+    const text: any[] = args.text ? args.text : ['default text'];
     const sx: any = args.sx || {};
     const WrappedIcon = IconComponent ? (
         <Box sx={{margin:"0 1em 0 0"}}>
@@ -21,20 +21,20 @@ const IconWithText = (args: any) => {
     ) : '';
     const WrappedText = (
         <Box sx={{display:"flex", flexDirection:"column", ...sx}}>
-            {text.map((line, idx) => <Typography key={idx}>{line}</Typography>)}
+            {text.map((line, idx) => <Box key={idx} sx={{marginTop:"3px"}}>{line}</Box>)}
         </Box>
     )
     return (
-        <Box sx={{display:"flex", width:"max-content"}}>
+        <Box sx={{display:"flex", width:"100%"}}>
             {WrappedIcon}
             {WrappedText}
         </Box>
     )
 }
 const ContactSection = (args: any) => {
-    const PhoneNumber = <IconWithText icon={PhoneIcon} text={["408-372-5492"]}/>
-    const Email = <IconWithText icon={EmailIcon} text={["jenlee@east-medicine.com"]} sx={{textDecoration:"underline"}}/>
-    const Address = <IconWithText icon={LocationOnIcon} text={["500 E Calaveras Blvd Suite 200", "Milpitas, CA 95035"]}/>
+    const PhoneNumber = <IconWithText icon={PhoneIcon} text={["408-372-5492"]}/>;
+    const Email = <IconWithText icon={EmailIcon} text={["jenlee@east-medicine.com"]} sx={{textDecoration:"underline"}}/>;
+    const Address = <IconWithText icon={LocationOnIcon} text={["500 E Calaveras Blvd Suite 200", "Milpitas, CA 95035"]}/>;
     return (<Box sx={{backgroundColor:"#F3F3F3", padding:"2em 0", border:"1px solid #D3D3D3"}}>
         <Box sx={{display:"flex", flexDirection:"column", justifyContent:"space-between", height:"115px", paddingLeft:"1em"}}>
             {PhoneNumber}
@@ -45,13 +45,11 @@ const ContactSection = (args: any) => {
 }
 
 const InsuranceSection = (args: any) => {
-    const InsuranceTitle = (
-        <Typography variant={"h5"} noWrap={true}>Insurance Accepted (PPO Only)</Typography>
-    );
-    const BlueShield = <IconWithText icon={ArrowRightIcon} text={["Blue Shield (select plans)"]}/>
-    const UnitedHealthcare = <IconWithText icon={ArrowRightIcon} text={["United Healthcare"]}/>
-    const Aetna = <IconWithText icon={ArrowRightIcon} text={["Aetna"]}/>
-    const Cigna = <IconWithText icon={ArrowRightIcon} text={["Cigna"]}/>
+    const InsuranceTitle = <Typography variant={"h5"} noWrap={true}>Insurance Accepted (PPO Only)</Typography>;
+    const BlueShield = <IconWithText icon={ArrowRightIcon} text={["Blue Shield (select plans)"]}/>;
+    const UnitedHealthcare = <IconWithText icon={ArrowRightIcon} text={["United Healthcare"]}/>;
+    const Aetna = <IconWithText icon={ArrowRightIcon} text={["Aetna"]}/>;
+    const Cigna = <IconWithText icon={ArrowRightIcon} text={["Cigna"]}/>;
     return (
         <Box sx={{backgroundColor:"#F3F3F3", padding:"2em 0 2em 1em", border:"1px solid #D3D3D3"}}>
             {InsuranceTitle}
@@ -65,13 +63,9 @@ const InsuranceSection = (args: any) => {
     )
 }
 
-const HoursSection = (args: any) => {
-    const HoursTitle = (
-        <Typography variant={"h5"} sx={{width:"max-content", paddingBottom:"1em"}}>Hours</Typography>
-    );
-    const HoursContent = (
-        <IconWithText text={["Monday - Friday: 830am - 7pm", "By appointment only"]}></IconWithText>
-    );
+const HoursSection = (args: {}) => {
+    const HoursTitle = <Typography variant={"h5"} sx={{width:"max-content", paddingBottom:"1em"}}>Hours</Typography>;
+    const HoursContent = <IconWithText text={["Monday - Friday: 830am - 7pm", "By appointment only"]}></IconWithText>;
     return (
         <Box sx={{backgroundColor:"#F3F3F3", padding:"2em 0 2em 1em", border:"1px solid #D3D3D3"}}>
             {HoursTitle}
@@ -80,15 +74,36 @@ const HoursSection = (args: any) => {
     );
 }
 
-const InfoSection = (args: any) => {
+const PriceSection = (args: {}) => {
+    const PriceTitle = <Typography variant={"h5"} sx={{width:"max-content", paddingBottom:"1em"}}>Cash Rate</Typography>;
+    const InitialText = <Typography sx={{marginTop:"-2px", overflowWrap:"break-word"}}>Initial Consultation + Acupuncture (90min) <b>$150</b></Typography>;
+    const FollowupText = <Typography sx={{marginTop:"-2px", overflowWrap:"break-word"}}>Followup Consultation + Acupuncture (60min) <b>$80</b></Typography>;
+    const InitialLine = (
+        <IconWithText icon={ArrowRightIcon} text={[InitialText]}></IconWithText>
+    );
+    const FollowupLine = (
+        <IconWithText icon={ArrowRightIcon} text={[FollowupText]}></IconWithText>
+    );
+    return (
+        <Box sx={{backgroundColor:"#F3F3F3", padding:"2em 0 2em 1em", border:"1px solid #D3D3D3"}}>
+            {PriceTitle}
+            {InitialLine}
+            {FollowupLine}
+        </Box>
+    );
+}
+
+const InfoSection = (args: {}) => {
     const ContactInfo = <ContactSection/>;
     const InsuranceInfo = <InsuranceSection/>;
     const HoursInfo = <HoursSection/>;
+    const PriceInfo = <PriceSection/>;
     return (
-        <Box sx={{minWidth: "340px", maxWidth:"510px"}}>
+        <Box sx={{minWidth: "300px", maxWidth:"510px"}}>
             {ContactInfo}
             {InsuranceInfo}
             {HoursInfo}
+            {PriceInfo}
         </Box>
     )
 }
